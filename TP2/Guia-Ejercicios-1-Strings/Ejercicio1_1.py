f8ce0cec38ef862cfead1a6253bfa304d130a32b
Masterjkk7 <65147060+Masterjkk7@users.noreply.github.com>
2021-06-04 17:39:01 -0300

    Delete TP2/Guia-Ejercicios-1-Strings directory

--- TP2/Guia-Ejercicios-1-Strings/Ejercicio1_1.py
+++
@@ -1,9 +0,0 @@
-nombre = input("Ingrese su nombre: ")
-
-inicial = nombre[0]
-print("Primera letra del nombre:",inicial)
-
-largo = str(len(nombre))
-print("Largo del nombre:",largo)
-
-#1.1. Realizar la carga del nombre de una persona y luego mostrar el primer caracter del nombre y la cantidad de letras que lo componen.
--- TP2/Guia-Ejercicios-1-Strings/Ejercicio1_3.py
+++
@@ -1,8 +0,0 @@
-email = input("Ingrese su Email: ")
-
-if "@" in email:
-    print("Su email es valido")
-else:
-    print("Su email no es valido")
-
-#1.3. Ingresar un mail por teclado. Verificar si el string ingresado contiene solo un caracter "@". Mostrar un mensaje informando dicha condición.
