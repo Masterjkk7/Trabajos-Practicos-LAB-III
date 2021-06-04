cc2262b2d417a8c7d5d2bfeefba2943c6c68a91b
Masterjkk7 <65147060+Masterjkk7@users.noreply.github.com>
2021-06-04 17:38:54 -0300

    Delete TP2/Guia-Ejercicios-1-Lists directory

--- TP2/Guia-Ejercicios-1-Lists/Ejercicio2_3.py
+++
@@ -1,7 +0,0 @@
-lista = ["Franco",9,10]
-promedio = (lista[1]+lista[2])/2
-
-print(lista[0],"tiene un promedio de:",promedio)
-
-#2.3. Definir una lista por asignación que almacene en la primer componente el nombre de un alumno y 
-# en las dos siguientes sus notas. Imprimir luego el nombre y el promedio de las dos notas.
