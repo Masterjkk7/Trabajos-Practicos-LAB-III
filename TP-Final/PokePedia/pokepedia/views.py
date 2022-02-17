9c1a957581d2f757594f006665589bc598029da4
Masterjkk7 <fran.gere88@gmail.com>
2022-02-17 19:37:20 -0300

    Muestra de JSON

--- TP-Final/PokePedia/pokepedia/views.py
+++ TP-Final/PokePedia/pokepedia/views.py
@@ -2,7 +2,7 @@ from asyncio.windows_events import NULL
 from ctypes.wintypes import BYTE
 from re import X
 from urllib.request import urlopen
-import requests
+import requests, json
 from django.shortcuts import redirect, render
 from .models import Pokemon, Tipo
 from django.core.exceptions import ObjectDoesNotExist
@@ -19,12 +19,12 @@ def get_pokemon(request):
             try:
                 p = Pokemon.objects.get(nombre__icontains=pokemon)
                 print("El pokemon "+p.nombre+" ya esta registrado")
-                context = {'pokemon' : p}
+                context = {'pokemon_db' : p}
                 return render(request, 'pokepedia/search.html', context)
             except ObjectDoesNotExist:
                 p = Pokemon.objects.get(numero__icontains=pokemon)
                 print("El pokemon "+p.nombre+" ya esta registrado")
-                context = {'pokemon' : p}
+                context = {'pokemon_db' : p}
                 return render(request, 'pokepedia/search.html', context)
         except:
             #return get_pokemon_api(request, pokemon)
@@ -41,7 +41,7 @@ def get_pokemon_api(request):
         try:
             response = requests.get('https://pokeapi.co/api/v2/pokemon/'+pokemon.lower())
             if response.status_code == 200:
-                print(response.json())
+                print(json.dumps(response.json(), indent=3))
                 name = response.json()["name"]
                 print("El pokemon "+name+" no esta registrado")
                 id = response.json()["id"]
@@ -86,7 +86,7 @@ def get_pokemon_api(request):
                 p.save()
                 x = Pokemon.objects.latest('id')
                 print("Indice de la tabla: "+str(x.id))
-                context = {'pokemon' : x}
+                context = {'pokemon' : x, 'response' : response.json()}
                 print("Se registro el pokemon en la base de datos") 
                 return render(request, 'pokepedia/search.html', context)
             else:
